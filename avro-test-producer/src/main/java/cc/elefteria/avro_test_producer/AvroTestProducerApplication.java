9d7f76ad93b5190e58a423b38c293436fe42400b
leroy <leroybacky@gmail.com>
2024-11-07 21:19:48 +0100

    init

---
+++ avro-test-producer/src/main/java/cc/elefteria/avro_test_producer/AvroTestProducerApplication.java
@@ -0,0 +1,13 @@
+package cc.elefteria.avro_test_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvroTestProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvroTestProducerApplication.class, args);
+	}
+
+}
---
+++ avro-test-producer/src/test/java/cc/elefteria/avro_test_producer/AvroTestProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.elefteria.avro_test_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvroTestProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
